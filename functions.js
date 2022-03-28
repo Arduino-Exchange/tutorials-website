9b64f5cecad139509be1d901e28deb73c0910b30
Pbaodoge <90094025+Pbaodoge@users.noreply.github.com>
2022-03-28 11:02:52 +0700

    less bloat script for redirecting to https protocol

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function httptohttps() {
     if (location.protocol === "http:") {
-        location.replace("https://tutorials.arduinoexchange.xyz");
+        location.replace(`https://${window.location.href}`);
     }
 }
