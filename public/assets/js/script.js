20f101cdc6159d840397430a1949aa51c1da1a74
Pbaodoge <banggb2008@gmail.com>
2022-07-27 12:46:24 +0700

    adding an hlml loading script for managing templates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,10 @@
 // Please change the HTTP to HTTPS using Cloudflares HTTP redirect feature. This is way faster than using JS.
+function loadEnding() {
+    //alert('sussy baka');
+    const id = document.querySelector(".ending");
+    id.innerHTML = `
+       <h3>Noticed something wrong? Submit an Issue or a Pull Request on <a href="https://github.com/Arduino-Exchange/tutorials-website">Github!</a></h3>
+       <h3>Arduino Exchange Tutorials is an open source, community driven project!</h3>
+       <br><br><br><br>
+       <footer>  © Arduino Exchange 2022 - Made with ❤️ by <a class="about-link" href="https://arduinoexchange.xyz/about">our developers.</a> </footer>`;
+}  
