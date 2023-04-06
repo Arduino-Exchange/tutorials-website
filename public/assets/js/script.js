14c4f81228ddd6d8b710a35c909684e92a5af3a8
Pbaodoge <banggb2008@gmail.com>
2023-04-06 15:07:14 +0700

    Some changes to 404 and main page

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,17 @@
-function loadEnding() {
+function loadassets() {
     //alert('sussy baka');
-    const id = document.querySelector(".ending");
-    id.innerHTML = `
-       <h3>Noticed something wrong? Submit an Issue or a Pull Request on <a href="https://github.com/Arduino-Exchange/tutorials-website">Github!</a></h3>
-       <h3>Arduino Exchange Tutorials is an open source, community driven project!</h3>
-       <br><br><br><br>
-       <footer>  © Arduino Exchange 2022 - Made with ❤️ by <a class="about-link" href="https://arduinoexchange.xyz/about">our developers.</a> </footer>`;
+    const ending = document.querySelector(".ending");
+    const menu = document.getElementsByClassName("navbar-container")[0];
+
+    fetch('/assets/templates/navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        menu.innerHTML = data;
+    });
+    
+    fetch('/assets/templates/ending.html')
+      .then(response => response.text())
+      .then(data => {
+        ending.innerHTML = data;
+    });
 }  
